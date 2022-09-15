4caf1b89dcf1a38db9b2d3034404c2a3683493b5
Onyekachukwu Okonji <onyekaokonji@gmail.com>
2022-09-15 02:52:43 +0100

    screenshots

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://abce1c98e452b49028ded9179b318111-312484883.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
